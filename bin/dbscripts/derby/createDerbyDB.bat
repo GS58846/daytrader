ba2c85fa7ed8c857dc0a2f9633d850eb9c2ea1cd
cjblythe <cjblythe@13f79535-47bb-0310-9956-ffa450edef68>
2007-02-27 18:23:30 +0000

    DAYTRADER-35 Move/update db creation scripts
    git-svn-id: http://svn.apache.org/repos/asf/geronimo/daytrader/trunk@512353 13f79535-47bb-0310-9956-ffa450edef68

---
+++ bin/dbscripts/derby/createDerbyDB.bat
@@ -0,0 +1,43 @@
+@REM   Licensed to the Apache Software Foundation (ASF) under one or more
+@REM   contributor license agreements.  See the NOTICE file distributed with
+@REM   this work for additional information regarding copyright ownership.
+@REM   The ASF licenses this file to You under the Apache License, Version 2.0
+@REM   (the "License"); you may not use this file except in compliance with
+@REM   the License.  You may obtain a copy of the License at
+@REM
+@REM      http://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM   Unless required by applicable law or agreed to in writing, software
+@REM   distributed under the License is distributed on an "AS IS" BASIS,
+@REM   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@REM   See the License for the specific language governing permissions and
+@REM   limitations under the License.
+
+@echo off
+
+if not "%JAVA_HOME%" == "" goto JAVA_OK
+@echo Please define the JAVA_HOME environment variable.
+@goto END
+:JAVA_OK
+
+if not "%GERONIMO_HOME%" == "" goto GERONIMO_OK
+@echo Please define the GERONIMO_HOME environment variable.
+@goto END
+:GERONIMO_OK
+
+set DERBY_PATH=%GERONIMO_HOME%\repository\org\apache\derby
+set CLASSPATH=%DERBY_PATH%\derby\10.1.3.1\derby-10.1.3.1.jar
+set CLASSPATH=%CLASSPATH%;%DERBY_PATH%\derbynet\10.1.3.1\derbynet-10.1.3.1.jar
+set CLASSPATH=%CLASSPATH%;%DERBY_PATH%\derbytools\10.1.3.1\derbytools-10.1.3.1.jar
+set CLASSPATH=%CLASSPATH%;%DERBY_PATH%\derbyclient\10.1.3.1\derbyclient-10.1.3.1.jar
+
+@echo "Invoking IJ command line tool to create the database and tables...please wait"
+
+%JAVA_HOME%\bin\java -Dij.driver=org.apache.derby.jdbc.ClientDriver -Dij.protocol=jdbc:derby://localhost:1527/ org.apache.derby.tools.ij < Table.ddl
+
+@REM The following command launches the interactive ij command line utility
+@REM java -Dij.driver=org.apache.derby.jdbc.ClientDriver -Dij.protocol=jdbc:derby://localhost:1527/ org.apache.derby.tools.ij 
+
+@echo Table creation complete
+ 
+:END
--- bin/dbscripts/derby/createDerbyDB.sh
+++ bin/dbscripts/derby/createDerbyDB.sh
@@ -15,18 +15,29 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-if [ "${GERONIMO_HOME}" = "" ]
+if [ "${JAVA_HOME}" = "" ]
 then
-  echo Please define the GERONIMO_HOME environment variable.
+  echo "Please define the JAVA_HOME environment variable."
   exit
 fi
-export G_PATH=${GERONIMO_HOME}/repository/org/apache/derby/
-export CLASSPATH=${G_PATH}/derby/10.1.1.0/derby-10.1.1.0.jar
-export CLASSPATH=${CLASSPATH}:${G_PATH}/derbynet/10.1.1.0/derbynet-10.1.1.0.jar
-export CLASSPATH=${CLASSPATH}:${G_PATH}/derbytools/10.1.1.0/derbytools-10.1.1.0.jar
-export CLASSPATH=${CLASSPATH}:${G_PATH}/derbyclient/10.1.1.0/derbyclient-10.1.1.0.jar
-export
+
+if [ "${GERONIMO_HOME}" = "" ]
+then
+  echo "Please define the GERONIMO_HOME environment variable."
+  exit
+fi
+
+export DERBY_PATH=${GERONIMO_HOME}/repository/org/apache/derby
+export CLASSPATH=${DERBY_PATH}/derby/10.1.3.1/derby-10.1.3.1.jar
+export CLASSPATH=${CLASSPATH}:${DERBY_PATH}/derbynet/10.1.3.1/derbynet-10.1.3.1.jar
+export CLASSPATH=${CLASSPATH}:${DERBY_PATH}/derbytools/10.1.3.1/derbytools-10.1.3.1.jar
+export CLASSPATH=${CLASSPATH}:${DERBY_PATH}/derbyclient/10.1.3.1/derbyclient-10.1.3.1.jar
+
 echo "Invoking IJ command line tool to create the database and tables...please wait"
-java -Dij.driver=org.apache.derby.jdbc.ClientDriver -Dij.protocol=jdbc:derby://localhost:1527/ org.apache.derby.tools.ij < derby.txt
-#java -Dij.driver=org.apache.derby.jdbc.ClientDriver -Dij.protocol=jdbc:derby://localhost:1527/ org.apache.derby.tools.ij 
+
+${JAVA_HOME}/bin/java -Dij.driver=org.apache.derby.jdbc.ClientDriver -Dij.protocol=jdbc:derby://localhost:1527/ org.apache.derby.tools.ij < Table.ddl
+
+# The following command launches the interactive ij command line utility
+#${JAVA_HOME}/bin/java -Dij.driver=org.apache.derby.jdbc.ClientDriver -Dij.protocol=jdbc:derby://localhost:1527/ org.apache.derby.tools.ij 
+
 echo "Table creation complete"
