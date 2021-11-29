88b127be67261f008309df6156d117d529fb8238
Muhammad Hafiz Hasan <mhafizhasan@gmail.com>
2021-11-29 22:54:11 +0800

    update

---
+++ codemr/daytrader/html/main_report/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,35 @@
+var matrix = [[0,4,0,0,3,0,0,1,5,7,0],[0,0,0,0,0,0,0,0,0,4,0],[0,2,0,0,4,0,0,2,2,1,0],[5,5,0,0,0,0,0,1,0,3,0],[0,1,0,0,0,0,0,1,0,0,0],[0,2,0,0,0,0,0,0,2,4,0],[0,0,0,0,0,0,0,0,0,0,0],[0,2,0,0,0,0,0,0,0,1,0],[0,3,0,0,3,0,0,3,0,3,0],[0,0,0,0,0,0,0,0,0,0,0],[0,8,8,0,0,0,0,0,0,11,0]]
+var packages = [{
+"name": " org.apache.geronimo.daytrader.javaee6.web", "color": " #3182bd"
+}
+,{
+"name": " org.apache.geronimo.daytrader.javaee6.entities", "color": " #6baed6"
+}
+,{
+"name": " org.apache.geronimo.daytrader.javaee6.web.ejb3", "color": " #9ecae1"
+}
+,{
+"name": " org.apache.geronimo.daytrader.javaee6.web.Beans", "color": " #c6dbef"
+}
+,{
+"name": " org.apache.geronimo.daytrader.javaee6.core.api", "color": " #e6550d"
+}
+,{
+"name": " org.apache.geronimo.daytrader.javaee6.web.prims", "color": " #fd8d3c"
+}
+,{
+"name": " org.apache.geronimo.daytrader.javaee6.web.oauth", "color": " #fdae6b"
+}
+,{
+"name": " org.apache.geronimo.daytrader.javaee6.core.beans", "color": " #fdd0a2"
+}
+,{
+"name": " org.apache.geronimo.daytrader.javaee6.core.direct", "color": " #31a354"
+}
+,{
+"name": " org.apache.geronimo.daytrader.javaee6.utils", "color": " #74c476"
+}
+,{
+"name": " org.apache.geronimo.daytrader.javaee6.web.prims.ejb3", "color": " #a1d99b"
+}
+];
